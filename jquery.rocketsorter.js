b220a145968b939ddd2b357fcd0680b39f3c3f66
Davi Reinke <davi.reinke@gmail.com>
2014-05-22 01:05:27 -0300

    Refactored! :)

--- jquery.rocketsorter.js
+++ jquery.rocketsorter.js
@@ -3,19 +3,19 @@
  *  Description: A recursive table sorter
  *  Author: Davi Reinke
  *  Contributor: Vinicius Ebersol
+ *  Repository: https://github.com/dreinke/jquery-rocketsorter
  *  License: MIT License
  *  http://www.opensource.org/licenses/mit-license.php
  */
 
 (function ($, window, document) {
-    var pluginName = "rocketSorter",
-        defaults = {
+    var defaults = {
             parameters: [{
                 coll: 0,
                 method: 'alphabetic',
                 reverse: false
             }],
-            customSorters: {
+            sorters: {
                 'alphabetic': function (a, b) {
                     return a > b ? 1 : -1;
                 },
@@ -43,59 +43,54 @@
         };
 
     function Plugin(element, options) {
-        this.element = $(element);
-        this.options = $.extend(true, defaults, options);
-        this._defaults = defaults;
-        this._name = pluginName;
+        this.sorters = $.extend(true, defaults.sorters, options.sorters);
+        this.parameters = $.extend(true, defaults.parameters, options.parameters);
 
-        this.sorters = {};
+        this.table = $(element);
+        this.tbody = this.table.find('tbody');
+        this.tableRows = this.tbody.find('tr');
 
-        this.init();
+        if (options.parameters) {
+            this.sortBy(this.parameters);
+        }
     }
 
     Plugin.prototype = {
-        init: function () {
-            var _this = this;
-
-            this.sorters = this.options.customSorters;
-
-            this.element.each(function () {
-                _this.parseTable(this, _this.options.parameters);
-            });
-        },
-
-        parseTable: function (table, parameters) {
+        sortBy: function (params) {
             var _this = this,
-                tableObj = $(table),
-                tbody = tableObj.find('tbody'),
-                tableRows = tbody.find('tr'),
-                rowsLength = tableRows.length,
+                parameters = $.extend(true, this.parameters, params),
+                rowsLength = this.tableRows.length,
                 rows = [],
                 i = 0;
 
             for (i = 0; i < rowsLength; i++) {
-                rows.push(tableRows[i]);
+                rows.push(this.tableRows[i]);
             }
 
             rows = this.sortRows(rows, parameters);
 
             for (i = 0; i < rowsLength; i++) {
-                tbody.append(rows[i]);
+                this.tbody.append(rows[i]);
             }
         },
 
         sortRows: function (rows, params) {
             var i,
-            _this = this,
+                _this = this,
                 parameters = [].concat(params),
                 param = parameters.shift(),
                 rowsLength = rows.length;
 
             rows.sort(function (rowA, rowB) {
-                var a = $(rowA.cells[param.coll]).html(),
-                    b = $(rowB.cells[param.coll]).html();
+                var a = $(rowA.cells[param.coll]),
+                    b = $(rowB.cells[param.coll]);
 
-                return _this.sorters[param.method](a, b);
+                if (a.data('value') && b.data('value'))
+
+                return _this.sorters[param.method](
+                    a.data('value')? a.data('value') : a.html(),
+                    b.data('value')? b.data('value') : b.html()
+                );
             });
 
             if (param.reverse) {
@@ -123,11 +118,7 @@
         }
     };
 
-    $.fn[pluginName] = function (options) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-            }
-        });
+    $.fn.rocketSorter = function (options) {
+        return new Plugin(this, options? options : {});
     };
 })(jQuery, window, document);
